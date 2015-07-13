c1888117e5949f8f11f26d044dab87d6811eaf2e
Joe Wang <joe.wang@careerbuilder.com>
2015-07-13 18:07:39 -0400

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root           'pages#index'
   get 'about' => 'pages#about'
 
-  devise_for :admins
   devise_for :users
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
--- db/migrate/20150705011532_add_lockable.rb
+++
@@ -1,7 +0,0 @@
-class AddLockable < ActiveRecord::Migration
-  def change
-    add_column :admins, :failed_attempts, :integer, default: 0
-    add_column :admins, :unlock_token, :string
-    add_column :admins, :locked_at, :datetime
-  end
-end
